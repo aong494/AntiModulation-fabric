6ebcaa3ba67deb0c3578a0d04d5d751669fa206f
aong494 <130423413+aong494@users.noreply.github.com>
2026-04-06 19:07:48 +0900

    Update VerifyPayload.java

--- src/main/java/aong/antimodulaiton/network/VerifyPayload.java
+++ src/main/java/aong/antimodulaiton/network/VerifyPayload.java
@@ -3,7 +3,6 @@ package aong.antimodulaiton.network;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-// 1.21.11 모장 공식 매핑에서는 ResourceLocation 대신 이걸 사용해!
 import net.minecraft.resources.Identifier;
 
 public record VerifyPayload() implements CustomPacketPayload {
